a4d75605b30ec11182ce666cc5369ad67c1a75ea
Roman <chulkovroma@mail.ru>
2021-04-29 21:07:55 +0300

    Cleaning

--- src/task_26/solution.py
+++ src/task_26/solution.py
@@ -1,3 +1,28 @@
+# Считаем входные ограничения
+S, N = map(int, input().split())
+
+# Ининциализируем нужные переменные
+files = [] # Массив файлов пользователей
+num_users = 0 # Макс. количество файлов пользователей
+max_file = 0 # Максимальный объем файла
+i = 0 # Индекс текущего элемента
+
+# Считаем файлы пользователей и добавим в массив
+for i in range(N):
+    files.append(int(input()))
+
+
+def BubbleSort(input_list):
+    for i in range(len(input_list) - 1):
+        is_sorted = True
+        for j in range(len(input_list) - 1):
+            if input_list[j] > input_list[j + 1]:
+                input_list[j], input_list[j + 1] = \
+                        input_list[j + 1], input_list[j]
+                is_sorted = False
+        if is_sorted:
+            break
+
 BubbleSort(files) # Отсортируем массив файлов
 
 # Заполним диск файлами
